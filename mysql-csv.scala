e25f98adc63efe36bbbe35aef50331286c798127
Valeriy Trofimov <ValTrofFuture@gmail.com>
2019-07-09 17:18:10 -0700

    add scripts for CSV generation

---
+++ mysql-csv.scala
@@ -0,0 +1,99 @@
+import java.io.File
+import java.io.PrintWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.sql.Timestamp
+
+// create output files
+val pwMySql = new PrintWriter(new File("mysql.sql"))
+val pwCsv = new PrintWriter(new File("bigdata.csv"))
+
+// create test database and table
+pwMySql.write("drop database if exists `bigdata_db`;\n")
+pwMySql.write("create database if not exists `bigdata_db`;\n")
+pwMySql.write("use `bigdata_db`;\n")
+pwMySql.write("drop table if exists `bigdata_tbl`;\n")
+
+// table settings
+val nRow = 100
+val nCol = 5
+val nRepeat = 10 // how many rows to use for repeating values
+
+// create table
+val mySqlSchema = new StringBuilder("create table `bigdata_tbl` (`time484c39f77ace4808a5a48f6b481bafbe` TIMESTAMP")
+val metrics = new StringBuilder();
+for (i <- 1 to nCol) {
+  mySqlSchema.append(
+    ", `intInc" + i + "` INTEGER" // incremental int
+    + ", `intRep" + i + "` INTEGER" // repeating int
+    + ", `dblInc" + i + "` DOUBLE" // incremental double
+    + ", `dblRep" + i + "` DOUBLE" // repeating double
+    + ", `strInc" + i + "` VARCHAR(30)" // incremental string
+    + ", `strRep" + i + "` VARCHAR(30)" // repeating string
+    + ", `decInc" + i + "` DECIMAL(10,4)" // incremental decimal
+    + ", `decRep" + i + "` DECIMAL(10,4)" // repeating decimal
+    + ", `boolInc" + i + "` BOOLEAN" // incremental bool
+    + ", `boolRep" + i + "` BOOLEAN" // repeating bool
+    + ", `smallInc" + i + "` SMALLINT" // incremental small
+    + ", `smallRep" + i + "` SMALLINT" // repeating small
+    + ", `dateCur" + i + "` DATE" // timestamp date
+    + ", `dateInc" + i + "` DATE" // incremental date
+  )
+}
+mySqlSchema.append(");\n")
+pwMySql.write(mySqlSchema.toString)
+
+// add rows
+val ts = Timestamp.valueOf("2019-01-01 00:00:00")
+val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+val timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+val timestampDruidFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+for (i <- 1 to nRow) {
+  val timestampLong = ts.getTime + i * 1000L
+  val timestamp = new Timestamp(timestampLong)
+  val timestampMySqlString = timestampFormat.format(timestamp)
+  val mySqlQuery = new StringBuilder("insert into `bigdata_tbl` values ('" + timestampMySqlString + "'")
+  val csvQuery = new StringBuilder(timestampMySqlString)
+  for (j <- 1 to nCol) {
+    mySqlQuery.append(
+      ", " + i // incremental int
+      + ", " + (i % nRepeat).toInt // repeating int
+      + ", " + (i / 100d) // incremental double
+      + ", " + (i % nRepeat / 100d) // repeating double
+      + ", 'strstrstrstrstrstr" + i + "'" // incremental string
+      + ", 'strstrstrstrstrstr" + (i % nRepeat).toLong + "'" // repeating string
+      + ", " + (i / 100d) // incremental decimal
+      + ", " + (i % nRepeat / 100d) // repeating decimal
+      + ", " + (i % 2) // incremental bool
+      + ", " + ((i / nRepeat).toLong % 2) // repeating bool
+      + ", " + (i % 32768).toShort // incremental short
+      + ", " + (i % 32768 % nRepeat).toShort // repeating short
+      + ", '" + dateFormat.format(timestampLong) + "'" // timestamp date
+      + ", '" + dateFormat.format(new Date(timestampLong + i * 86400L * 1000L)) + "'" // incremental date
+    )
+    csvQuery.append(
+      "," + i // incremental int
+      + "," + (i % nRepeat).toInt // repeating int
+      + "," + (i / 100d) // incremental double
+      + "," + (i % nRepeat / 100d) // repeating double
+      + ",'strstrstrstrstrstr" + i + "'" // incremental string
+      + ",'strstrstrstrstrstr" + (i % nRepeat).toLong + "'" // repeating string
+      + "," + (i / 100d) // incremental decimal
+      + "," + (i % nRepeat / 100d) // repeating decimal
+      + "," + (i % 2) // incremental bool
+      + "," + ((i / nRepeat).toLong % 2) // repeating bool
+      + "," + (i % 32768).toShort // incremental short
+      + "," + (i % 32768 % nRepeat).toShort // repeating short
+      + ",'" + dateFormat.format(timestampLong) + "'" // timestamp date
+      + ",'" + dateFormat.format(new Date(timestampLong + i * 86400L * 1000L)) + "'" // incremental date
+    )
+  }
+  mySqlQuery.append(");\n")
+  pwMySql.write(mySqlQuery.toString)
+  csvQuery.append("\n")
+  pwCsv.write(csvQuery.toString)
+}
+
+pwMySql.close
+pwCsv.close
+
